f23117bf981a7eb6b608abbe22acbed11887c62b
lsVals <victoralfonsolarasantiago1@gmail.com>
2025-03-12 17:17:20 -0600

    Fix: puerto dinamico para Railway

--- servidor/index.js
+++ servidor/index.js
@@ -5,6 +5,7 @@ app.get('/', (req, res) => {
     res.send('¡Servidor funcionando correctamente!');
 });
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(Servidor corriendo en el puerto ${PORT});
 });
