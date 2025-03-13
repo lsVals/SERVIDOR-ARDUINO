f026e03874086f8c0030dccdd6d55cc26303c75c
lsVals <victoralfonsolarasantiago1@gmail.com>
2025-03-12 18:17:42 -0600

    Correccion del package json

--- servidor/index.js
+++ servidor/index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(Servidor corriendo en el puerto ${PORT});
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
